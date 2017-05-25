00fe5cf8c2e8373ef24b5ca7fb90cf57d21c45f3
kitchell <lindseykitchell21@gmail.com>
2017-05-25 12:02:23 -0400

    add scripts

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # app-datanormalize
 application to normalize the bvalues and flip the bvecs if necessary
+
+still need to edit: clean.sh, package.json
---
+++ main.m
@@ -0,0 +1,72 @@
+function [] = main()
+% normalizes the bvals and splits the bvecs
+
+% load config.json
+config = loadjson('config.json');
+
+% Parameters used for normalization
+params.single_shells = config.shells;
+params.thresholds.b0_normalize    = 200;
+params.thresholds.bvals_normalize = 100;
+
+
+%% Normalize HCP files to the VISTASOFT environment
+
+bvals.val = dlmread(config.bvals);
+
+% Round the numbers to the closest thousand 
+% This is necessary because the VISTASOFT software does not handle the B0
+% when they are not rounded.
+[bvals.unique, ~, bvals.uindex] = unique(bvals.val);
+if ~isequal(bvals.unique, params.single_shells)
+    bvals.unique(bvals.unique <= params.thresholds.b0_normalize) = 0;
+    bvals.unique  = round(bvals.unique./params.thresholds.bvals_normalize) ...
+        *params.thresholds.bvals_normalize;
+    bvals.valnorm = bvals.unique( bvals.uindex );
+    dlmwrite('dwi_normalized.bvals',bvals.valnorm);
+else
+    bvals.valnorm = bvals.val;
+    dlmwrite('dwi_normalized.bvals',bvals.valnorm);
+end
+
+
+%load bvecs
+bvecs = dlmread(config.bvecs);
+
+%params
+params.x_flip = config.xflip;
+params.y_flip = config.yflip;
+params.z_flip = config.zflip;
+
+if ~(size(bvecs,1) == 3), bvecs = bvecs'; end
+
+if params.x_flip
+    bvecs(1,:) = -bvecs(1,:);
+end
+if params.y_flip
+    bvecs(2,:) = -bvecs(2,:);
+end
+if params.z_flip
+    bvecs(3,:) = -bvecs(3,:);
+end
+
+% may not be necessary depending on the data structure?
+
+if params.x_flip && params.y_flip && params.z_flip
+    dlmwrite(dwi_x_y_z_flipped.bvecs, bvecs);
+elseif params.x_flip && params.y_flip && ~params.z_flip
+    dlmwrite(dwi_x_y_flipped.bvecs, bvecs);
+elseif params.x_flip && ~params.y_flip && params.z_flip
+    dlmwrite(dwi_x_z_flipped.bvecs, bvecs);   
+elseif ~params.x_flip && params.y_flip && params.z_flip
+    dlmwrite(dwi_y_z_flipped.bvecs, bvecs);
+elseif params.x_flip && ~params.y_flip && ~params.z_flip
+    dlmwrite(dwi_x_flipped.bvecs, bvecs);
+elseif ~params.x_flip && params.y_flip && ~params.z_flip
+    dlmwrite(dwi_y_flipped.bvecs, bvecs);
+elseif ~params.x_flip && ~params.y_flip && params.z_flip
+    dlmwrite(dwi_z_flipped.bvecs, bvecs);
+end
+end
+
+
