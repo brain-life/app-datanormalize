c5c221a3f23468b8548dc8d3694390097d1a27ca
kitchell <lindseykitchell21@gmail.com>
2017-05-25 12:29:31 -0400

    update

--- main.m
+++ main.m
@@ -1,4 +1,14 @@
 function [] = main()
+
+if isempty(getenv('SCA_SERVICE_DIR'))
+    disp('setting SCA_SERVICE_DIR to pwd')
+    setenv('SCA_SERVICE_DIR', pwd)
+end
+
+disp('loading paths')
+addpath(genpath('/N/u/hayashis/BigRed2/git/jsonlab'))
+
+
 % normalizes the bvals and splits the bvecs
 
 % load config.json
